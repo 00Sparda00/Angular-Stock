0441d51065088293fed59e9a304e4560e6586992
Panlop <panlop.developer@gmail.com>
2024-07-05 23:55:05 +0700

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    dotnet_api_url: 'http://localhost:8081/api/',
-    dotnet_api_url_image: 'http://localhost:8081/uploads/',
+    dotnet_api_url: 'https://dotnetstockapi-1h7n.onrender.com//api/',
+    dotnet_api_url_image: 'https://dotnetstockapi-1h7n.onrender.com//uploads/',
     base_url: '',
     token: '',
     version: '1.0',
